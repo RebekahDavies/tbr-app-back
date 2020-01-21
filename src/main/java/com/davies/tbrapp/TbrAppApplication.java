b1b80e1c50d995ce1a55334610e35db5aee157fd
Rebekah Davies <rebekah@backbase.com>
2020-01-21 10:24:27 +0000

    Initial commit

---
+++ src/main/java/com/davies/tbrapp/TbrAppApplication.java
@@ -0,0 +1,13 @@
+package com.davies.tbrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davies/tbrapp/TbrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davies.tbrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
